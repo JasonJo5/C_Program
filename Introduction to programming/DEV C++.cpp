0daf23e19b5c07a68c98d2d10b5d3352c420df46
JasonJo5 <jasonjoevanka@gmail.com>
2023-09-07 23:41:03 +0900

    task

---
+++ Introduction to programming/DEV C++.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	cout<<"Welcome to woosong university"<<endl;
+	cout<<"Welcome to woosong university";
+}
---
+++ Introduction to programming/Introduction to programming/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+/*Practice c++ basic*/
+
+int main()
+{
+    int Myage = 18;
+    cout<< "Hello guys " << "I am actually" <<  Myage << " years old";
+    return 0;
+}
